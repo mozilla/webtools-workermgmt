126e3fa83fcdd8da15661289f03842b302bd10ca
skeen@mozilla.com <skeen@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-06-22 22:08:49 +0000

    restructuring to meet K3 class file layout

---
+++ modules/bugzilla/classes/Ldap/Mock_Ldap.php
@@ -0,0 +1,44 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Mock_Ldap library.
+ *
+ *
+ * @package    Mock_Ldap
+ * @author     skeen@mozilla.org
+ * @license    http://www.opensource.org/licenses/mit-license.php
+ */
+class Mock_Ldap_Core extends Ldap_Core {
+
+  private $manager_list =
+    '{"bob1@somewhere.com":{
+        "cn":"Bob One",
+        "title":"Number One Bob",
+        "bugzilla_email":"bug-bob1@somewhere.com"
+    },
+    {"bob2@somewhere.com":{
+        "cn":"Bob Two",
+        "title":"Number Two Bob",
+        "bugzilla_email":"bug-bob2@somewhere.com"
+    }';
+  
+
+  public function  __construct($config, $credentials) {
+    kohana::log('debug',"**USING MockLdap**");
+    $this->manager_list = $config['mock_ldap_manager_list']
+            ? $config['mock_ldap_manager_list']
+            : $this->manager_list;
+    return parent::__construct($config, $credentials);
+  }
+
+  public function employee_list($type) {
+    kohana::log('debug',"Called MOCK ".__METHOD__);
+    return json_decode($this->manager_list, true);
+  }
+  public function employee_attributes($ldap_email) {
+    kohana::log('debug',"Called MOCK ".__METHOD__);
+    $manager_list = json_decode($this->manager_list, true);
+    return isset($manager_list[$ldap_email])
+      ? $manager_list[$ldap_email]
+      : array();
+  }
+}
