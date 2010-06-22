06756b7d6a73c29e41375ba3cfa05d98605b76d0
skeen@mozilla.com <skeen@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-06-22 18:53:54 +0000

    added =id (input('first_name=id') syntax so id="..." is rendered where needed

--- modules/bugzilla/views/pages/bz_login.php
+++ modules/bugzilla/views/pages/bz_login.php
@@ -4,12 +4,12 @@
       <?php echo form::hidden('form_token', $_SESSION['form_token'] = uniqid()); ?>
       <div class="input">
         <?php echo form::label('bz_username', 'Bugzilla E-mail Address:'); ?>
-        <?php echo form::input('bz_username', $bz_username); ?>
+        <?php echo form::input('bz_username=id', $bz_username); ?>
         <?php client::validation('bz_username'); ?>
       </div>
       <div class="input">
         <?php echo form::label('bz_password', 'Bugzilla Password:'); ?>
-        <?php echo form::password('bz_password', $bz_password); ?>
+        <?php echo form::password('bz_password=id', $bz_password); ?>
         <?php client::validation('bz_password'); ?>
       </div>
       <div>
